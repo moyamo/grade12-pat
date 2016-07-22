5fb5793ceb9eb0ad4a17ea5a59272a610262956c
moyamo <moyamodehacker@gmail.com>
2016-07-22 21:51:53 +0200

    Add UI for Billing Patient

--- Grade12PAT/src/grade12pat/BillPatient.java
+++ Grade12PAT/src/grade12pat/BillPatient.java
@@ -5,17 +5,24 @@
  */
 package grade12pat;
 
+import java.util.List;
+import java.util.Vector;
+
 /**
  *
  * @author yaseen
  */
 public class BillPatient extends javax.swing.JFrame {
-
+    RcdAppointments appointment;
+    Session session;
     /**
      * Creates new form BillPatient
      */
-    public BillPatient(RcdAppointments patient, Session session) {
+    public BillPatient(RcdAppointments appointment, Session session) {
         initComponents();
+        this.appointment = appointment;
+        this.session = session;
+        fillInformation();
     }
 
     /**
@@ -32,12 +39,18 @@ public class BillPatient extends javax.swing.JFrame {
         cmbBillingItem = new javax.swing.JComboBox<>();
         jScrollPane1 = new javax.swing.JScrollPane();
         lstBills = new javax.swing.JList<>();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         lblPatientName.setText("Patient Name");
 
         jButton1.setText("Add");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         cmbBillingItem.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
@@ -47,6 +60,13 @@ public class BillPatient extends javax.swing.JFrame {
             public String getElementAt(int i) { return strings[i]; }
         });
         jScrollPane1.setViewportView(lstBills);
+
+        jButton2.setText("Back");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -58,10 +78,13 @@ public class BillPatient extends javax.swing.JFrame {
                     .addComponent(lblPatientName)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton1)
-                            .addComponent(cmbBillingItem, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(59, 59, 59)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 236, Short.MAX_VALUE)))
+                            .addComponent(cmbBillingItem, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jButton1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jButton2)))
+                        .addGap(25, 25, 25)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 260, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -74,18 +97,61 @@ public class BillPatient extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(cmbBillingItem, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButton1)
+                            .addComponent(jButton2)))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(81, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String selected = (String) cmbBillingItem.getSelectedItem();
+        List<RcdBillingItems> bills = session.sqlQuery("SELECT * FROM BillingItems", RcdBillingItems.class);
+        for (RcdBillingItems b: bills) {
+            if (selected.equals(b.getDescription() + " R" + b.getPrice())) {
+                session.getEntityManager().getTransaction().begin();
+                RcdBillingHistory bh = new RcdBillingHistory(session.nextId("BillingHistory"));
+                bh.setConsultationid(this.appointment);
+                bh.setDescription(b.getDescription());
+                bh.setPrice(b.getPrice());
+                bh.setPatientid(appointment.getPatientid());
+                bh.setTime(appointment.getTime());
+                session.getEntityManager().persist(bh);
+                session.getEntityManager().getTransaction().commit();
+                break;
+            }
+        }
+        session.getEntityManager().refresh(appointment);
+        fillInformation();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        session.showAppointments();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox<String> cmbBillingItem;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblPatientName;
     private javax.swing.JList<String> lstBills;
     // End of variables declaration//GEN-END:variables
+
+    private void fillInformation() {
+        cmbBillingItem.removeAllItems();
+        List<RcdBillingItems> bills = session.sqlQuery("SELECT * FROM BillingItems", RcdBillingItems.class);
+        for (RcdBillingItems b: bills) {
+            cmbBillingItem.addItem(b.getDescription() + " R" + b.getPrice());
+        }
+        lblPatientName.setText(appointment.getPatientid().getFirstnames() + " "+ appointment.getPatientid().getSurname());
+        Vector accrued = new Vector();
+        for (RcdBillingHistory bh : appointment.getRcdBillingHistoryCollection()) {
+            accrued.add(bh.getDescription() + " R" + bh.getPrice());
+        }
+        lstBills.setListData(accrued);
+    }
 }
